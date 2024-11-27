60df04fede25e4441f93d982d0a3315a77c64303
MyUsername <rosanio.c@gmail.com>
2024-11-27 16:11:16 +0100

    npm i bootstrap + import bootstrap su main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,39 @@ import { useState } from 'react'
 import './App.css'
 import { tasks } from './assets/data/tasksData'
 
+
+
 function App() {
+
+  const listArray = (arr) => {
+    return (
+      <ul>
+        {arr.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+    );
+  };
+
+  const filterArray = (arr, state) => {
+    const filteredTasks = arr.filter(item => item.state != state)
+    return filteredTasks;
+  };
+
+  const filteredTasks = filterArray(tasks, 'completed');
+
 
   return (
     <>
-      <h1>My React project</h1>
+      <header className='header'>
+        <h1>My React project</h1>
+      </header>
+
       <p>{tasks[0].state}</p>
 
-      <ul>
-        {tasks.map((task) => <li key={task.id}>{task.title}</li>)}
-      </ul>
+      {listArray(tasks)}
+
+      {listArray(filteredTasks)}
     </>
   )
 }
